5d225f7b32af4644260ee3517214e2892d9e6b45
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-14 12:40:28 +0300

    Ex2
    Added the second exercise

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of c: ");
+            int c = int.Parse(Console.ReadLine());
+            if (a==0||b==0||c==0)
+            {
+                Console.WriteLine("The product of the threee numbers is 0");
+            }
+            else if ((a<0&&b>0&&c>0)||(b<0&&c>0&&a>0)||(c<0&&b>0&&a>0)||(a<0&&b<0&&c<0))
+            {
+                Console.WriteLine("The product of the numbers is negative");
+            }
+            else
+            {
+                Console.WriteLine("The product of the numbers is positive");
+            }
+            Console.ReadLine();
+        }
+    }
+}
