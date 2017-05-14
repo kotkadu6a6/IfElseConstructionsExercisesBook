7702896e29b0d9082c4e723b9e73e0bd4c0d5e6b
Bozhidar Todorov <bozhidarrr@gmail.com>
2017-05-14 12:24:15 +0300

    Ex.1
    Added the first exercise

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = new int();
+            if (a>b)
+            {
+                c = a;
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("a = {0}, b = {1}",a,b);
+            Console.ReadLine();
+
+        }
+    }
+}
